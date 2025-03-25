92ffcd762015c612d1a8d92d5c9e46dd746042c2
Steven <stevenmb1995@gmail.com>
2025-03-24 19:36:10 -0500

    typescript refactor for setting up the redux store and defining types

---
+++ redux/types.ts
@@ -0,0 +1,40 @@
+/**
+ * Action type constants for the data management Redux slice. These
+ * constants ensure that dispatch and reducer logic remain in sync.
+ */
+export const SET_DATA_X = 'SET_DATA_X';
+export const SET_DATA_Y = 'SET_DATA_Y';
+export const SET_ROWS = 'SET_ROWS';
+export const RESET_DATA = 'RESET_DATA';
+export const RANDOMIZE_DATA = 'RANDOMIZE_DATA';
+
+// Action interfaces
+export interface SetDataXAction {
+  type: typeof SET_DATA_X;
+  payload: { index: number; value: any };
+}
+
+export interface SetDataYAction {
+  type: typeof SET_DATA_Y;
+  payload: { index: number; value: any };
+}
+
+export interface SetRowsAction {
+  type: typeof SET_ROWS;
+  payload: { value: number };
+}
+
+export interface ResetDataAction {
+  type: typeof RESET_DATA;
+}
+
+export interface RandomizeDataAction {
+  type: typeof RANDOMIZE_DATA;
+}
+
+export type DataActionTypes =
+  | SetDataXAction
+  | SetDataYAction
+  | SetRowsAction
+  | ResetDataAction
+  | RandomizeDataAction;
